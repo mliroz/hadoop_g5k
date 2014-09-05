466791e5e2625b19b0ee31bf191e71a3066466cf
Miguel Liroz <miguel.liroz_gistau@inria.fr>
2014-09-05 17:37:18 +0200

    1. Creation of setup.py and reestructuration to include scripts in its own directory.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup,find_packages
+
+setup (
+  name = "hadoop_g5k",
+  version = "0.1",
+
+  packages = ["hadoop_g5k"],
+  scripts = ["scripts/hg5k.py", "scripts/hadoop_engine.py"],
+
+  install_requires=["execo", " networkx"],
+
+  # PyPI
+  author = 'Miguel Liroz Gistau',
+  author_email = 'miguel.liroz_gistau@inria.fr',
+  description = "A collection of scripts and packages that help in the deployment"
+    " and experimental evaluation of Hadoop in Grid5000.",
+  url = "https://github.com/mliroz/hadoop_g5k",
+  license = "BSD",  
+  keywords = "hadoop g5k grid5000 execo",
+  
+)
