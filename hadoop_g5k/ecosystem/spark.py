ead4fe3c9f534dddfd9a68624235f1a705aa9223
Laudin Alessandro Molina T <laudin.molina@gmail.com>
2015-11-06 16:30:42 +0100

    Bug fix: spark force_clean stops running instances in all hosts.
    Spark function __force_clean stooped running instances only in the master
    host. This fix enables the function to stop running instances in all hosts.

--- hadoop_g5k/ecosystem/spark.py
+++ hadoop_g5k/ecosystem/spark.py
@@ -729,7 +729,7 @@ class SparkCluster(object):
 
         force_kill = False
         for h in self.hosts:
-            proc = SshProcess("jps", self.master)
+            proc = SshProcess("jps", h)
             proc.run()
 
             ids_to_kill = []
@@ -743,6 +743,10 @@ class SparkCluster(object):
                 ids_to_kill_str = ""
                 for pid in ids_to_kill:
                     ids_to_kill_str += " " + pid
+
+                logger.warn(
+                    "Killing running Spark processes in host %s" %
+                    style.host(h.address.split('.')[0]))
 
                 proc = SshProcess("kill -9" + ids_to_kill_str, h)
                 proc.run()
