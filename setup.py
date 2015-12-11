842cc3c88483f5b0c1655d4148e80710f51a14ab
Miguel Liroz <miguel.liroz_gistau@inria.fr>
2015-12-11 10:53:31 +0100

    Debugging of setup script.

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="hadoop_g5k",
-    version="0.1",
+    version="0.5",
 
-    packages=["hadoop_g5k", "hadoop_g5k/util", "hadoop_g5k/engine",
-              "hadoop_g5k/ecosystem"],
+    packages=find_packages(),
     scripts=["scripts/hg5k", "scripts/hadoop_engine", "scripts/spark_g5k",
              "scripts/mahout_g5k"],
 
-    install_requires=["execo", " networkx"],
+    install_requires=["execo", "networkx"],
 
     # PyPI
     author='Miguel Liroz Gistau',
@@ -20,6 +19,6 @@ setup(
                 "deployment and experimental evaluation of Hadoop in Grid5000.",
     url="https://github.com/mliroz/hadoop_g5k",
     license="BSD",
-    keywords="hadoop spark g5k grid5000 execo",
+    keywords="hadoop spark hive g5k grid5000 execo",
   
 )
